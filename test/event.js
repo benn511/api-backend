5e8541b4aabc63b557cd45dca8fea627a215930b
benn511 <bgomez190@gmail.com>
2020-12-25 17:30:50 -0600

    unit test for route was not added correctly

---
+++ test/event.js
@@ -0,0 +1,20 @@
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("/GET events", () => {
+  it("it should GET all the events", (done) => {
+    chai
+      .request(server)
+      .get("/db_api/read/events")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+});
