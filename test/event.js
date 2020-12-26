d25f991dbb5324c6ac1c487a645a94438bb0eeab
Benito Gomez <bgomez@Benitos-MacBook-Pro.local>
2020-12-26 13:55:25 -0600

    added psuedo code for CREATE, READ, unit tests

--- test/event.js
+++ test/event.js
@@ -6,6 +6,9 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+/* ----------CREATE---------- */
+
+/* ----------READ--------- */
 describe("/GET events", () => {
   it("it should GET all the events", (done) => {
     chai
@@ -14,7 +17,37 @@ describe("/GET events", () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a("object");
+        //should have property events?
         done();
       });
   });
 });
+
+//unit test for read ONE event
+//should have status 200
+//should be an object
+//should have a property event
+//should be of length 1
+
+//unit test for create route WORKING
+//describe /post
+//should have status 201//Created response
+//should be an object
+//should have a property event
+//should have a property msg
+
+//unit test for create route where you try to create a dupe
+//describe /post
+//should have status 409//Conflict with current state of the server
+//should be an object
+//should have an error with property of msg
+
+//unit test for create route when missing a parameter
+//describe /post
+//should have a status 206//Partial content error code
+//should be an object
+//should have a property msg
+//should have an error msg
+
+/* ---------UPDATE---------- */
+/* --------DESTROY---------- */
